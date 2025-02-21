cd60a8fad43d342ef1328f383f976e6f20fbd6e2
Alok Singh <aloksingh36757@gmail.com>
2025-02-21 17:43:33 +0530

    employee management System

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("", home),
+    path("", home),
     path('index/',home),
     path('time/',currrent_time_date),
     path('about/',about_page),
-    path('services/',services)
+    path('services/',services),
+    path('emp/',include('emp.urls'))
 ]
