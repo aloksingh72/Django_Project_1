d44861d520aaa5f08dd9fd2535849c51ab9a62a5
Alok Singh <aloksingh36757@gmail.com>
2025-02-21 09:59:01 +0530

    first django app completed

--- myapp/website/models.py
+++ myapp/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    email = models.EmailField(unique=True)
+   
+ 
