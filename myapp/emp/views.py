0fa6c19d5e459febcc839fd453b79dcf9bb93f2b
Alok Singh <aloksingh36757@gmail.com>
2025-02-24 18:29:03 +0530

    forms validated

--- myapp/emp/views.py
+++ myapp/emp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Emp
+from .models import Emp,Testimonial
+from .forms import FeedbackForm
 # Create your views here.
 
 def emp_home(request):
@@ -78,3 +79,25 @@ def do_update_emp(request,emp_id):
 
          return redirect("/emp/home/")
         
+
+def testimonials(request):
+        testi = Testimonial.objects.all()
+
+        return render(request, "emp/testimonials.html",{
+                'testi':testi
+        })
+
+def feedback(request):
+        if request.method == 'POST':
+              form = FeedbackForm(request.POST)
+              if form.is_valid():
+                      print(form.cleaned_data['email'])
+                      print(form.cleaned_data['name'])
+                      print(form.cleaned_data['feedback'])
+                      print("Data is saved")
+              else:
+                      return render(request,"emp/feedback.html",{'form':form})
+                            
+        else:
+                form = FeedbackForm()
+                return render(request,"emp/feedback.html",{'form':form})
