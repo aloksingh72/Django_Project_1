1c58d7a7ac49b0a63ad8377fe24907bd6dd5fec4
Alok Singh <aloksingh36757@gmail.com>
2025-02-25 11:51:50 +0530

    custom user Model in Django

--- myapp/emp/models.py
+++ myapp/emp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 # Create your models here.
@@ -18,7 +19,24 @@ class Testimonial(models.Model):
     name = models.CharField(max_length = 200)
     testimonial = models.TextField()
     picture = models.ImageField(upload_to = "testimonials/")
-    rating = models.IntegerField(max_length = 1)
+    rating = models.IntegerField()
 
     def __str__(self):
         return self.testimonial
+    
+
+
+# Django default user model using AbstractUser
+class CustomUser(AbstractUser):
+    profile_picture =models.ImageField(upload_to='profile_pics/',blank=True,null=True)
+    date_of_birth = models.DateField(blank = True, null = True)
+
+     # Resolve related_name conflicts
+    groups = models.ManyToManyField(Group, related_name="customuser_groups", blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name="customuser_permissions", blank=True)
+
+
+
+    def __str__(self):
+        return self.username
+
